bbfb16d60917cb64095f4ec36c09ff6b4ea8949e
Gregory Higley <greg@prosumma.com>
2025-01-05 01:05:14 -0500

    genjwk

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Lib
+import Servant.Auth.Server
+
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Base64 as Base64
+import qualified RIO.ByteString as ByteString
 
 main :: IO ()
-main = someFunc
+main = generateKey >>= ByteString.putStr . Base64.encode . ByteString.toStrict . Aeson.encode >> ByteString.putStr "\n"
